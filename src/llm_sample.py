b6c85968f3c4baa22a87949f236556e5057452f8
coela-oss <to@coela.org>
2025-03-05 13:42:17 +0900

    Intel Openvino

--- src/llm_sample.py
+++ src/llm_sample.py
@@ -1,29 +1,20 @@
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from gpt4all import GPT4All
+from openvino import Core
 
-tokenizer = AutoTokenizer.from_pretrained(
-    "NousResearch/DeepHermes-3-Llama-3-8B-Preview",
-    cache_dir="./mnt/models/"
+core = Core()
+devices = core.available_devices
+print("Available devices:", devices)
+
+model_path = "./mnt/models/models--microsoft--Phi-3-mini-4k-instruct-gguf/"
+model_file = "./mnt/models/models--microsoft--Phi-3-mini-4k-instruct-gguf/Phi-3-mini-4k-instruct-q4.gguf"
+model = GPT4All(
+    "Phi-3-mini-4k-instruct-q4.gguf",
+    device="gpu",
+    model_path=model_path,
+    ngl=8,
+    n_threads=8,
+    n_ctx=4096,
+    allow_download=False
 )
-
-model = AutoModelForCausalLM.from_pretrained(
-    "NousResearch/DeepHermes-3-Llama-3-8B-Preview",
-    cache_dir="./mnt/models/"
-)
-
-messages = [
-    {
-        "role": "system",
-        "content": "You are a deep thinking AI, you may use extremely long chains of thought to deeply consider the problem and deliberate with yourself via systematic reasoning processes to help come to a correct solution prior to answering. You should enclose your thoughts and internal monologue inside <think> </think> tags, and then provide your solution or response to the problem."
-    },
-    {
-        "role": "user",
-        "content": "What is y if y=2*2-4+(3*2)"
-    }
-]
-
-input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors='pt').to("cuda")
-generated_ids = model.generate(input_ids, max_new_tokens=2500, temperature=0.8, repetition_penalty=1.1, do_sample=True, eos_token_id=tokenizer.eos_token_id)
-print(f"Generated Tokens: {generated_ids.shape[-1:]}")
-response = tokenizer.decode(generated_ids[0], skip_special_tokens=True, clean_up_tokenization_space=True)
-print(f"Response: {response}")
+output = model.generate("こんにちは", max_tokens=1, temp=0)
+print(output)
